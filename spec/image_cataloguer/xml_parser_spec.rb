54828c078ee05bb22c522c43402cd1f58f25d045
Brett Clark <clarkbab@gmail.com>
2016-08-01 19:53:35 +1000

    Added more.

--- spec/image_cataloguer/xml_parser_spec.rb
+++ spec/image_cataloguer/xml_parser_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe ImageCataloguer::XMLParser do
         @url = "http://take-home-test.herokuapp.com/api/v1/works.xml"
       end
 
-      it "should not raise any errors on valid input" do
+      it "should not raise an error" do
         VCR.use_cassette "api_v1_works" do
           expect{
             described_class.parse_URL(@url)
@@ -45,6 +45,39 @@ RSpec.describe ImageCataloguer::XMLParser do
             described_class.parse_URL(@url)
           }.to raise_error described_class::SyntaxError, "XML file has incorrect format."
         end
+      end
+    end
+  end
+
+  describe "build_images" do
+    context "when given a valid array of XML nodes" do
+      before (:each) do
+        VCR.use_cassette "api_v1_works" do
+          url = "http://take-home-test.herokuapp.com/api/v1/works.xml"
+          @works = XMLParser.parse_URL(url)
+        end
+      end
+
+      it "should not raise an error" do
+        expect{
+          XMLParser.build_images(@works)
+        }.to_not raise_error
+      end
+
+      it "should return an array of images" do
+        images = XMLParser.build_images(@works)
+        expect(images.first.class).to eq ImageCataloguer::Image
+      end
+
+      it "should filter out invalid images" do
+        expect(@works.length).to eq 14
+        images = XMLParser.build_images(@works)
+        expect(images.length).to eq 12
+      end
+
+      it "should return an array of length 12" do
+        images = XMLParser.build_images(@works)
+        expect(images.length).to eq 12
       end
     end
   end
